7ab978481378ad19afac46fadd5f6d61820d5e39
Your Name <adityaraj642003@gmail.com>
2024-06-03 14:35:27 +0530

    updated

--- utils/database.js
+++ utils/database.js
@@ -1,18 +1,15 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const databaseConnection = async () => {
-    try {
-        const uri = process.env.MONGO_URI;
-        console.log(`Connecting to database at: ${uri}`);  // Debugging line
-        await mongoose.connect(uri, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('Database connected successfully');
-    } catch (error) {
-        console.error('Database connection error:', error);
-        process.exit(1);  // Exit process with failure
-    }
+dotenv.config({
+    path:"../.env"
+})
+
+const databaseConnection = () => {
+    mongoose.connect(process.env.MONGO_URI).then(()=>{
+        console.log("mongodb connected successfully");
+    }).catch((error)=>{
+        console.log(error);
+    })
 };
-
-module.exports = databaseConnection;
+export default databaseConnection;
